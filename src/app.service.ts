a3f68cfc43de51764fd269b6dd366ebab42c7ecf
edson dev <edsondev@enova.com>
2025-11-10 10:04:13 -0300

    doc: mostrar versao

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Versão 251110-0955 separar BD dev(docker local) e stg(neon.com)';
   }
 }
